17e1ab66151acb2412ecfb21e133ec9a2e24f38a
andressa-mb <andressamesquita26@gmail.com>
2024-04-01 15:55:13 -0300

    nova alteração

--- index.js
+++ index.js
@@ -87,6 +87,6 @@ app.delete('/livros/:id', async (req, res) => {
     }
 });
     
-app.listen("https://biblioteca-livros-back.vercel.app/", () => {
+app.listen(8080, () => {
     console.log("Servidor funcionando na porta 8080")
 })
